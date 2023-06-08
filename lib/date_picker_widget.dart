b18b5377866ccdb9e83fe506a40bfd7a4765e15d
Michael H <michael.hejl@laggelee.se>
2023-06-08 16:10:51 +0200

    .

--- lib/date_picker_widget.dart
+++ lib/date_picker_widget.dart
@@ -262,6 +262,6 @@ class DatePickerController {
     final startDate = new DateTime(_datePickerState!.widget.startDate.year, _datePickerState!.widget.startDate.month, _datePickerState!.widget.startDate.day);
 
     int offset = date.difference(startDate).inDays;
-    return (offset * _datePickerState!.widget.width) + (offset * 6) + 82;
+    return (offset * _datePickerState!.widget.width) + (offset * 6) - 82;
   }
 }
