9b711755d938d2d46e5735133107044941d7e25d
Michael H <michael.hejl@laggelee.se>
2023-06-09 11:01:04 +0200

    .

--- lib/date_widget.dart
+++ lib/date_widget.dart
@@ -52,7 +52,7 @@ class DateWidget extends StatelessWidget {
             children: <Widget>[
               checkYear()
                   ? Text(new DateFormat("MMM", locale).format(date).toUpperCase(), style: monthTextStyle)
-                  : Text(new DateFormat("MMM -yy", locale).format(date).toUpperCase(), style: monthTextStyle),
+                  : Text(new DateFormat("MMM''yy", locale).format(date).toUpperCase(), style: monthTextStyle),
               Text(date.day.toString(), // Date
                   style: dateTextStyle),
               Text(new DateFormat("E", locale).format(date).toUpperCase(), // WeekDay
